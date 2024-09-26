ba595a2ac44073c770140d2efeb740738e34fe3a
joycee2523 <camillegeocallo05@gmai.com>
2024-09-26 14:54:21 +0800

    database

---
+++ src/joy2d/JOY2D.java
@@ -0,0 +1,57 @@
+
+package joy2d;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class JOY2D {
+
+//Connection Method to SQLITE
+public static Connection connectDB() {
+   Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
+            con = DriverManager.getConnection("jdbc:sqlite:testapp.db"); // Establish connection
+            System.out.println("Connection Successful");
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e);
+        }
+        return con;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        connectDB();
+        
+        
+        System.out.print("First Name: ");
+        String fn = sc.nextLine();
+        System.out.print("Last Name: ");
+        String ln = sc.nextLine();
+        System.out.print("Email: ");
+        String ems = sc.nextLine();
+        System.out.print("Status: ");
+        String sts = sc.nextLine();
+        
+        String sql = "INSERT INTO tbl_students (s_fname, s_lname, s_email, s_status) VALUES (? , ?, ?, ?)";
+        
+        try {
+                
+                Connection con = connectDB();
+                PreparedStatement pst = con.prepareStatement(sql);
+                       pst.setString(1, fn);
+                       pst.setString(2, ln);
+                       pst.setString(3, ems);
+                       pst.setString(4, sts);
+                       pst.executeUpdate();
+                       System.out.println("Inserted Successfully!");
+        }catch (SQLException ex) {
+            System.out.println("Connection Error: "+ex.getMessage());
+        }
+    }
+    }
+
